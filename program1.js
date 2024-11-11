d8202ca46b3a692ce14933aae1278dea92e45f56
AmitRaj2001 <rajamit.contact@gmail.com>
2024-11-11 14:16:04 +0530

    modified:   program1.js 	modified:   program2.js

--- program1.js
+++ program1.js
@@ -1,7 +1,40 @@
 const getTotalIsles = function (grid) {
-
-
   // write your code here
+  if (!grid || grid.length === 0) return 0;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islandCount = 0;
+
+  // marking the connected land as visited
+  const dfs = (i, j) => {
+    
+    if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] === 'W') {
+      return;
+    }
+
+    
+    grid[i][j] = 'W';
+
+    
+    dfs(i + 1, j); // down
+    dfs(i - 1, j); // up
+    dfs(i, j + 1); // right
+    dfs(i, j - 1); // left
+  };
+
+  
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L') {
+        
+        islandCount++;
+        dfs(i, j); 
+      }
+    }
+  }
+
+  return islandCount;
 
 };
 
